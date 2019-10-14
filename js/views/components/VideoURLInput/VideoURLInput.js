8e400e83418b0fe6a4e828acd5855f065f3771ef
Dominic Boggio <dominicboggio@gmail.com>
2019-10-14 17:36:51 -0400

    Added Separate button for Viewing Room
    Called "QueueButton"

--- js/views/components/VideoURLInput/VideoURLInput.js
+++ js/views/components/VideoURLInput/VideoURLInput.js
@@ -2,18 +2,28 @@ let m = require("mithril");
 
 let Textbox = require("./Textbox");
 let SearchButton = require("./SearchButton");
+let QueueButton = require("./QueueButton")
 
 /*
     Textbox and button for entering a URL and creating a 
     viewing room to watch the video
 */
+function onLandingPage(){
+      if(window.location.hash==""){
+          return m(SearchButton);
+      }else{
+          return m(QueueButton);
+      }
+}
+console.log(onLandingPage());
 let VideoURLInput = {
     view: ()=>{
         return m("", {class:"VideoURLInput"},
             m("", {class:"wrapper"},[
                 /* Create vnodes for the textbox and search button */
                 m(Textbox),
-                m(SearchButton)
+                //m(SearchButton)
+                onLandingPage()
             ])
         );
     }
