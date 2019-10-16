4cbe68928c3183bd9736bca43195189a6c8ce748
Brad Hoose <bjhoose@mtu.edu>
2019-10-16 19:38:30 -0400

    Chat Tab button functionaility is still bugged.

--- js/views/components/ChatBox/ChatListTabButton.js
+++ js/views/components/ChatBox/ChatListTabButton.js
@@ -3,13 +3,11 @@ var ChatBox = require("./ChatBox");
 
 var ChatListTabButton = {
     view: (vnode) => {
-        console.log(ChatBox);
         return m("input[type=button]", {class: "chatlisttabbutton", value: "Users!", 
             onclick: () => {
-                // This variable controls the visibility of the user list.
-                console.log(ChatBox)
-                ChatBox.setChatList();
                 console.log(ChatBox);
+                // This variable controls the visibility of the user list.
+                ChatBox.setChatList();
                //return ChatBox.ChatTab;
             }
         });
--- js/views/components/ChatBox/ChatTabButton.js
+++ js/views/components/ChatBox/ChatTabButton.js
@@ -5,9 +5,10 @@ var ChatTabButton = {
     view: (vnode) => {
         return m("input[type=button]", {class: "chattabbutton", value: "Chat!", 
             onclick: () => {
+                console.log(ChatBox);
                 // ChatTab is a variable in ChatBox which controls the visibility of the chat history.
                 // When this boolean is true, the chat history is visible.
-                ChatBox.ChatTab = true;
+                ChatBox.setChatList();
                 //return Chatbox.ChatTab;
             }
         });
