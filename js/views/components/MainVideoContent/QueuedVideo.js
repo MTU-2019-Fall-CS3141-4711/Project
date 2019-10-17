baf5398ec00235bb27206b8581fa8cac09a5d484
Dominic Boggio <dominicboggio@gmail.com>
2019-10-17 19:17:26 -0400

    Added pseudo-functionality to the queue button
    Queue button can recieve input,, now we just need to change /what/ input it's using.

--- js/views/components/MainVideoContent/QueuedVideo.js
+++ js/views/components/MainVideoContent/QueuedVideo.js
@@ -5,15 +5,17 @@ var m = require("mithril");
 */
 // Default YouTube Thumbnail URL (standard definition)
 // https://img.youtube.com/vi/<insert-youtube-video-id-here>/sddefault.jpg
+var title ="";
+var user = "";
 var QueuedVideo = {
-    view: () => {
+    view: (vnode) => {
         return m("div", {class: "queued-video"},[
             m("img", { // contains the image 
                 "src":"https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg","alt":"Queued Video","height":"52","width":"92.44"
             }),
             m("div", {"class":"text-container"},[ // contains the following two text elements
-                m("div", {"class":"video-title"}, "Queued Video!"),
-                m("div", {"class":"queued-by"}, "Queued By!")
+                m("div", {"class":"video-title"}, vnode.attrs.videoTitle),
+                m("div", {"class":"queued-by"}, vnode.attrs.queueUser)
             ])
         ]);
     }
--- js/views/components/VideoURLInput/Textbox.js
+++ js/views/components/VideoURLInput/Textbox.js
@@ -2,14 +2,17 @@ let m = require("mithril");
 
 /* Textbox at top of page for video URL */
 let Textbox = {
-
+    getText: () =>{
+        return document.getElementById("URLInput").nodeValue;
+    },
     /* Create input of type text */
-    view: () =>{
+    view: (vnode) =>{
         return m("input[type=text]", {
 
             /* The light grey helper text in the text box */
+            id: "URLInput",
             placeholder:"Enter a YouTube URL"
-        
+            
         });
     }
 }
