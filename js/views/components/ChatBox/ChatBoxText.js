149a1bc487b37413dbe91a7af158830666821a9a
parsonswy <parsonswy@gmail.com>
2019-10-25 14:25:33 -0400

    Clear chatbox & send on enter

--- js/views/components/ChatBox/ChatBoxText.js
+++ js/views/components/ChatBox/ChatBoxText.js
@@ -1,15 +1,31 @@
 var m = require("mithril");
 
-var message;
+var Chat = require("./../../../models/Chat");
 
 var ChatBoxText = {
-    ChatMessage: null,
-    view: (vnode) => {
-        return m("textarea", {class:"chatboxtext", 
-        onkeyup: (e) => {
-            ChatBoxText.ChatMessage = e.target.value;
-        }
-    });
+    textarea: null,
+    view: () => {
+        ChatBoxText.textarea = m("textarea", {class:"chatboxtext",
+            onkeyup: (e) => {
+                if(e.keyCode == 13){
+                    Chat.sendMessage( ChatBoxText.getTextAreaMessage() );
+                    ChatBoxText.clearTextArea();
+                }
+            }
+        });
+        return ChatBoxText.textarea;
+    },
+    getTextAreaMessage: () => {
+        return ChatBoxText.textarea.dom.value;
+    },
+    clearTextArea: () => {
+        /**
+         * TODO: 
+         * Is this the proper way to do this in Mithril?
+         * It seems like there should be a way to do it without touching
+         * the DOM directly
+         */
+        ChatBoxText.textarea.dom.value = "";
     }
 }
 
