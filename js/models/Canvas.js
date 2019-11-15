13b03588b996b4f4026b61f4abd333642f01176c
parsonswy <parsonswy@gmail.com>
2019-11-14 21:27:40 -0500

    Working local user Canvas

--- js/models/Canvas.js
+++ js/models/Canvas.js
@@ -1,26 +1,33 @@
-var ToolbarState = require("./ToolbarState");
-var User = require("./User");
-
-var YTVideoFrame = require("./YTVideoIframe");
-
 var Canvas = {
+    isDrawing: false,
+    context: null,
+    width: 1,
+    height: 1,
+    sX: 0,
+    sY: 0,
     construct: () => {
 
     },
 
     registerCanvas: (c) => {
         Canvas.context = c.getContext("2d");
+        Canvas.context.strokeStyle = 'black';
+        Canvas.context.lineWidth = 4;
+        Canvas.width = c.clientHeight;
+        Canvas.height = c.clientWidth;
     },
 
-    click: (e) => {
-        if(ToolbarState.getTool() == ToolbarState.POINTER && User.isHost){
-            YTVideoFrame.togglePlaybackLocal();
-        }
-    },
+    paint: (sX, sY, eX, eY) => {
+        Canvas.context.beginPath();
+        Canvas.context.moveTo(sX, sY);
+        Canvas.context.lineTo(eX, eY);
+        Canvas.context.stroke();
+        Canvas.context.closePath();
 
-    drawRect: () => {
-        Canvas.context.drawRect(150, 150, 100, 100);
+        Canvas.sX = eX;
+        Canvas.sY = eY;
     }
+
 }
 
 module.exports = Canvas;
