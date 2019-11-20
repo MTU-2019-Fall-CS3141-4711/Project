40a0921660c17f59400d6a205bbd97f130242eaa
parsonswy <parsonswy@gmail.com>
2019-11-20 12:18:26 -0500

    Replace delete function

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,5 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.onUserPresenceChange = require("./UserPresence");
-exports.deleteRoom = require("./DeleteRoom");
+//exports.onUserPresenceChange = require("./UserPresence");
 exports.Clear = require("./Clear");
